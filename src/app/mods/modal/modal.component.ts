43c0b61f289f404d5fe48553eacd2c739db8f2b7
Captain Storm <CaptainStorm21@users.noreply.github.com>
2021-07-06 11:20:29 -0700

    modal install ng-bootstrap

--- src/app/mods/modal/modal.component.ts
+++ src/app/mods/modal/modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-modal',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModalComponent implements OnInit {
 
-  constructor() { }
 
+  closeModal: string;
+
+  constructor(private modalService: NgbModal) {}
   ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  // tslint:disable-next-line: typedef
+  triggerModal(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((res) => {
+      this.closeModal = `Closed with: ${res}`;
+    }, (res) => {
+      this.closeModal = `Dismissed ${this.getDismissReason(res)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
